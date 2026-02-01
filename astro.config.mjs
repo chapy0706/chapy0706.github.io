6af57ee19c3e3e141ca1d517b2c868853d3ffb88
chapy0706 <chapy76@icloud.com>
2026-02-01 15:43:16 +0900

    #1 依存関係の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import remarkAlerts from 'remark-alerts';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    remarkPlugins: [remarkAlerts],
+  },
   vite: {
     plugins: [tailwindcss()]
   }
